0c25652f4209837077df479ed0d73010fa612d67
José P. Ramos <jpramos2910@gmail.com>
2024-10-20 14:49:05 -0300

    NgIf mostrar figura y mejora en UI
    Se incluye ngIf para mostrar la figura geometrica según la que se haya seleccionado, además se elimina div container que me estaba centrando contenido verticalmente

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {
   IonHeader, IonToolbar, IonTitle, IonContent,
   IonSelect, IonSelectOption 
 } from '@ionic/angular/standalone';
+import { TrianguloComponent } from "../triangulo/triangulo.component";
+import { CirculoComponent } from "../circulo/circulo.component";
 
 @Component({
   selector: 'app-home',
@@ -10,10 +13,30 @@ import {
   styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [
+    CommonModule,
     IonHeader, IonToolbar, IonTitle, IonContent,
-    IonSelect, IonSelectOption
-  ],
+    IonSelect, IonSelectOption,
+    TrianguloComponent, CirculoComponent
+],
 })
 export class HomePage {
   constructor() {}
+
+  // variable que almacena la figura geometrica seleccionada
+  figuraGeometrica: string|undefined = undefined;
+
+  esCirculo(): boolean {
+    // retorna si la figura seleccionada es circulo
+    return this.figuraGeometrica == "circulo";
+  }
+
+  esTriangulo(): boolean {
+    // retorna si la figura seleccionada es triangulo
+    return this.figuraGeometrica == "triangulo";
+  }
+
+  handleFiguraGeometricaChange(event:any){
+    // setea la figura geometrica seleccionada en la variable
+    this.figuraGeometrica = event.target.value;
+  }
 }
