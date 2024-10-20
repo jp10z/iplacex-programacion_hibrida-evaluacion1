4f62da9748ca59b9c98cc6819d6a488d59a2b251
José P. Ramos <jpramos2910@gmail.com>
2024-10-20 14:14:55 -0300

    se agrega ion-select al home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent,
+  IonSelect, IonSelectOption 
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    IonSelect, IonSelectOption
+  ],
 })
 export class HomePage {
   constructor() {}
