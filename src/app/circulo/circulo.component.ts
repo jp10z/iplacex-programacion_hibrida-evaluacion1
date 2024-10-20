1118d1e1078d2d488808f6dc01e893091768f419
José P. Ramos <jpramos2910@gmail.com>
2024-10-20 16:41:52 -0300

    Se agrega evento para calcular el perimetro

--- src/app/circulo/circulo.component.ts
+++ src/app/circulo/circulo.component.ts
@@ -2,15 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import {
   IonList, IonItem, IonInput, IonCard, IonCardHeader, IonCardTitle,
-  IonCardSubtitle, IonCardContent
-} from '@ionic/angular/standalone';
+  IonCardSubtitle, IonCardContent, IonButton } from '@ionic/angular/standalone';
+import { Circulo } from '../modelo/circulo';
 
 @Component({
   selector: 'app-circulo',
   templateUrl: './circulo.component.html',
   styleUrls: ['./circulo.component.scss'],
   standalone: true,
-  imports: [
+  imports: [IonButton, 
     ReactiveFormsModule, IonList, IonItem, IonInput,
     IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard
   ]
@@ -21,8 +21,25 @@ export class CirculoComponent  implements OnInit {
 
   ngOnInit() {}
 
+  textoResultado: string = "";
+
   formulario = new FormGroup({
-    radio: new FormControl(undefined, [Validators.required])
+    radio: new FormControl(0, [Validators.required, Validators.min(0)])
   })
+
+  get radio() {
+    return this.formulario.get("radio")?.value ?? 0;
+  }
+
+  calcularPerimetro(){
+    // validamos formulario
+    if (!this.formulario.valid) {
+      this.textoResultado = "Error al procesar el cálculo, por favor valide el formulario";
+      return;
+    }
+    // instanciamos circulo y procesamos el calculo
+    const circulo = new Circulo(this.radio);
+    this.textoResultado = `El perímetro es ${circulo.calcularPerimetro()}`
+  }
 
 }
--- src/app/triangulo/triangulo.component.ts
+++ src/app/triangulo/triangulo.component.ts
@@ -2,15 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import {
   IonList, IonItem, IonInput, IonCard, IonCardHeader, IonCardTitle,
-  IonCardSubtitle, IonCardContent
-} from '@ionic/angular/standalone';
+  IonCardSubtitle, IonCardContent, IonButton } from '@ionic/angular/standalone';
+import { TrianguloEscaleno } from '../modelo/triangulo-escaleno';
 
 @Component({
   selector: 'app-triangulo',
   templateUrl: './triangulo.component.html',
   styleUrls: ['./triangulo.component.scss'],
   standalone: true,
-  imports: [
+  imports: [IonButton, 
     ReactiveFormsModule, IonList, IonItem, IonInput,
     IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard
   ]
@@ -21,10 +21,35 @@ export class TrianguloComponent  implements OnInit {
 
   ngOnInit() {}
 
+  textoResultado: string = "";
+
   formulario = new FormGroup({
-    ladoA: new FormControl(undefined, [Validators.required]),
-    ladoB: new FormControl(undefined, [Validators.required]),
-    ladoC: new FormControl(undefined, [Validators.required])
+    ladoA: new FormControl(0, [Validators.required]),
+    ladoB: new FormControl(0, [Validators.required]),
+    ladoC: new FormControl(0, [Validators.required])
   })
+
+  get ladoA() {
+    return this.formulario.get("ladoA")?.value ?? 0;
+  }
+
+  get ladoB() {
+    return this.formulario.get("ladoB")?.value ?? 0;
+  }
+
+  get ladoC() {
+    return this.formulario.get("ladoC")?.value ?? 0;
+  }
+
+  calcularPerimetro(){
+    // validamos formulario
+    if (!this.formulario.valid) {
+      this.textoResultado = "Error al procesar el cálculo, por favor valide el formulario";
+      return;
+    }
+    // instanciamos circulo y procesamos el calculo
+    const triangulo = new TrianguloEscaleno(this.ladoA, this.ladoB, this.ladoC);
+    this.textoResultado = `El perímetro es ${triangulo.calcularPerimetro()}`
+  }
 
 }
