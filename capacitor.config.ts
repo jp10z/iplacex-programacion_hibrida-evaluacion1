4384b6df05f84771aa8972297a677f2066fb8ca2
José P. Ramos <jpramos2910@gmail.com>
2024-10-20 13:21:19 -0300

    Codigo inicial
    Se agrega código limpio, justo después de la creación con la extensión de Ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.evaluacion1',
+  appName: 'evaluacion1',
+  webDir: 'www'
+};
+
+export default config;
