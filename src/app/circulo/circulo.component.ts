4b5b60f4813cdc637ae6baf6a00b7a53240094a2
José P. Ramos <jpramos2910@gmail.com>
2024-10-20 16:07:55 -0300

    se agregan formularios y diseño básico de los componentes de figuras

--- src/app/circulo/circulo.component.ts
+++ src/app/circulo/circulo.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  IonList, IonItem, IonInput, IonCard, IonCardHeader, IonCardTitle,
+  IonCardSubtitle, IonCardContent
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-circulo',
   templateUrl: './circulo.component.html',
   styleUrls: ['./circulo.component.scss'],
   standalone: true,
+  imports: [
+    ReactiveFormsModule, IonList, IonItem, IonInput,
+    IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard
+  ]
 })
 export class CirculoComponent  implements OnInit {
 
   constructor() { }
 
   ngOnInit() {}
+
+  formulario = new FormGroup({
+    radio: new FormControl(undefined, [Validators.required])
+  })
 
 }
--- src/app/triangulo/triangulo.component.ts
+++ src/app/triangulo/triangulo.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  IonList, IonItem, IonInput, IonCard, IonCardHeader, IonCardTitle,
+  IonCardSubtitle, IonCardContent
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-triangulo',
   templateUrl: './triangulo.component.html',
   styleUrls: ['./triangulo.component.scss'],
   standalone: true,
+  imports: [
+    ReactiveFormsModule, IonList, IonItem, IonInput,
+    IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard
+  ]
 })
 export class TrianguloComponent  implements OnInit {
 
   constructor() { }
 
   ngOnInit() {}
+
+  formulario = new FormGroup({
+    ladoA: new FormControl(undefined, [Validators.required]),
+    ladoB: new FormControl(undefined, [Validators.required]),
+    ladoC: new FormControl(undefined, [Validators.required])
+  })
 
 }
